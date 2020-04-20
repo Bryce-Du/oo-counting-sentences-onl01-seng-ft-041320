e0660ff57316224f9ab6c8fd872d248610568203
Bryce Dumas <bryce.dumas@gmail.com>
2020-04-20 20:42:19 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -17,6 +17,7 @@ class String
   def count_sentences
     sentence_array = self.split(/[.,!,?]/)
     selectences = sentence_array.select do |sentence|
+      puts sentence
       sentence != "!" && sentence != "?" && sentence != "."
     end
     selectences.length
